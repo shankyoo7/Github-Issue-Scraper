4d66b8c270e822850acf0a7f9b294a72394e686c
shankyoo7 <61755941+shankyoo7@users.noreply.github.com>
2021-11-19 23:04:10 +0530

    Add files via upload

---
+++ reposPage.js
@@ -0,0 +1,37 @@
+
+const cheerio=require("cheerio");
+const request = require("request");
+const getIssuesPageHtml=require("./issue")
+
+function getReposPageHtml(url,topic){
+    request(url,cb);
+    function cb(err,response,html){
+        if(err)
+        {
+            console.log(err);
+        }
+        else if (response.statusCode == 404) {
+            console.log("page not found");
+        }
+        else{
+           // console.log(html);
+           getReposLink(html);
+        }
+    }
+    function getReposLink(html){
+        let $=cheerio.load(html);
+        let headingsArr= $(".f3.color-fg-muted.text-normal.lh-condensed");
+        //console.log(topic);
+        for(let i=0;i<8;i++){
+            let twoAnchors=$(headingsArr[i]).find("a");
+            let link=$(twoAnchors[1]).attr("href");
+            //console.log(link);
+            let fullLink = `https://github.com${link}/issues`;
+            let repoName = link.split("/").pop();
+            getIssuesPageHtml(fullLink,topic,repoName);
+
+        }
+    }
+}
+
+module.exports=getReposPageHtml;
